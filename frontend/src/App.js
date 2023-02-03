381879a336c6119f0b3dd61ed92816601995a1ed
d3meter <mark.demeter3@gmail.com>
2023-02-03 12:00:04 +0100

    create project

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from "react";
+import './App.css';
+
+function App() {
+  const [nasaData, setNasaData] = useState([]);
+
+  const apiKey = "QYSDCrsuNdQpx6YY9Yg2eO9RBWDIVwpWkwhwYWi8";
+  let fetchUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&start_date=2022-01-01&end_date=2022-02-01`;
+
+  useEffect(() => {
+    fetch(fetchUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setNasaData(data);
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
