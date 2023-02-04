c86b5a15de7981b41e9e24330091596c567f7fae
d3meter <mark.demeter3@gmail.com>
2023-02-04 15:47:32 +0100

    install react-calendar

--- frontend/src/components/MonthCard.jsx
+++ frontend/src/components/MonthCard.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import DayCard from "./DayCard";
-import './css/MonthCard.css'
+import './css/MonthCard.css';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 function MonthCard({ nasaData }) {
+  const [date, setDate] = useState(new Date())
 
   return (
     <div className="MonthCard">
+      <Calendar onChange={setDate} value={date} />
       {Array.from(nasaData).map((data, i) => (
         <DayCard key={i} data={data} />
       ))}
