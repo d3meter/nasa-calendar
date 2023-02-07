b465f119c31844877cc2f5d064146c16828bcde7
d3meter <mark.demeter3@gmail.com>
2023-02-07 15:02:37 +0100

    add spinner, made calendar apperance

--- frontend/src/components/DayCard.jsx
+++ frontend/src/components/DayCard.jsx
@@ -1,20 +1,38 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./css/DayCard.css";
+import spinner from "../pub-imgs/spinner.gif";
 
 function DayCard({ data }) {
+  const [loading, setLoading] = useState(false);
+
   const date = data.date;
+  const dateSplit = date.split("-");
+  const day = dateSplit[2];
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
 
   return (
-    <div className="DayCard">
-      <Link to={"/" + date}>
-        <div className="day-img">
-          <img src={data.url} alt="no" />
-        </div>
-        <p>{data.title}</p>
-        <p>{data.date}</p>
-      </Link>
-    </div>
+    <Link className="DayCard" to={"/" + date}>
+      <p className="day-tag">{day}</p>
+      <div className="day-img">
+        {!loading ? (
+          data.media_type !== "image" ? (
+            <iframe src={data.url} title={data.title} frameborder="0"></iframe>
+          ) : (
+            <img src={data.url} alt="no" />
+          )
+        ) : (
+          <img src={spinner} alt="spinner" />
+        )}
+      </div>
+      <p>{data.title}</p>
+    </Link>
   );
 }
 
