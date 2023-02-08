9619bbbafa0e9fac8a251711c35f0f2c1e951c5f
d3meter <mark.demeter3@gmail.com>
2023-02-08 11:58:08 +0100

    fix style dayDetail and Main

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import DayDetail from "./DayDetail";
-import "./css/Main.css";
 
 function Main() {
   return (
     <div className="Main">
-      <div className="main-container">
         <DayDetail />
-      </div>
     </div>
   );
 }
