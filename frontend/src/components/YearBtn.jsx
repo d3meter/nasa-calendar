d553a0343d984ab52ff7a6283af69855b1ec333b
d3meter <mark.demeter3@gmail.com>
2023-03-20 16:11:59 +0100

    reduced data because of hosting

--- frontend/src/components/YearBtn.jsx
+++ frontend/src/components/YearBtn.jsx
@@ -13,7 +13,7 @@ function YearBtn({ changeYear, yearSelect }) {
         onChange={changeYear}
         disableScrollLock={true}
       >
-        <MenuItem value={1995}>1995</MenuItem>
+{/*         <MenuItem value={1995}>1995</MenuItem>
         <MenuItem value={1996}>1996</MenuItem>
         <MenuItem value={1997}>1997</MenuItem>
         <MenuItem value={1998}>1998</MenuItem>
@@ -27,7 +27,7 @@ function YearBtn({ changeYear, yearSelect }) {
         <MenuItem value={2006}>2006</MenuItem>
         <MenuItem value={2007}>2007</MenuItem>
         <MenuItem value={2008}>2008</MenuItem>
-        <MenuItem value={2009}>2009</MenuItem>
+        <MenuItem value={2009}>2009</MenuItem> */}
         <MenuItem value={2010}>2010</MenuItem>
         <MenuItem value={2011}>2011</MenuItem>
         <MenuItem value={2012}>2012</MenuItem>
